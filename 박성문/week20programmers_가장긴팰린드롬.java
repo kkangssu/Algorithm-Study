1399d70a14ac48472677125c23638ec579d0cc25
SungMoonPark <taim831@naver.com>
2025-02-02 23:35:11 +0900

    박성문_가장긴팰린드롬.java

---
+++ 박성문/week20programmers_가장긴팰린드롬.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                int left = i;
+                int right = j;
+                if(j-i+1 <= answer){
+                    continue;
+                }
+                boolean no = false;
+                while(left < right){
+                    if(left < 0 || right >= s.length()){
+                        break;
+                    }
+                    if(s.charAt(left++) != s.charAt(right--)){
+                        no = true;
+                        break;
+                    }
+                }
+                if(!no){
+                    answer =Math.max(answer, j-i+1);
+                }
+            }
+        }
+
+        return answer;
+    }
+}

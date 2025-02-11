2ea2891d50e432f67793dd405a48145007a999d6
sujin <unikang16@gmail.com>
2025-02-11 09:04:42 +0900

    [Week 21] Programmers 징검다리건너기 / 40분

---
+++ week21/programmers_징검다리건너기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] stones, int k) {
+        int right = Integer.MIN_VALUE;
+        int left = Integer.MAX_VALUE;
+
+        for(int i = 0; i < stones.length; i++){
+            right = Math.max(right, stones[i]);
+            left = Math.min(left, stones[i]);
+        }
+        int mid = 0;
+        while(left <= right){
+            mid = (left+right)/2;
+            if(cross(mid, stones, k)){
+                left = mid+1;
+            }
+            else right = mid-1;
+        }
+
+        return right+1;
+    }
+    static boolean cross(int mid, int[] stones, int k){
+        int cnt = 0;
+        for(int i = 0; i < stones.length; i++){
+            if(stones[i]-mid <= 0) cnt++;
+            else cnt = 0;
+
+            if(cnt >= k) return false;
+        }
+        return true;
+    }
+}

6e21f45a4ba51033a2c18d4f67932733bdad4213
SungMoonPark <taim831@naver.com>
2025-03-07 08:54:07 +0900

    programmers_오픈채팅방.java

--- week24/박성문/programmers_오픈채팅방.java
+++ week24/박성문/programmers_오픈채팅방.java
@@ -4,9 +4,13 @@ class Solution {
         HashMap<String,String> hash = new HashMap<>();
         String[][] map = new String[record.length][2];
         
+        int cnt = 0;
         for(int i = 0; i < record.length; i++) {
             String[] tmp  = record[i].split(" ");
-
+            
+            if(!tmp[0].equals("Change"))
+                cnt++;
+            
             map[i][0] = tmp[0];
             map[i][1] = tmp[1];
             
@@ -15,17 +19,17 @@ class Solution {
             
         }
         
-        List<String> answer = new LinkedList<>();
-        
+        String[] answer = new String[cnt];
+        int idx = 0;
         for(int i = 0; i < record.length; i++) {
             if(map[i][0].equals("Change"))
                 continue;
             
-            answer.add((map[i][0].equals("Enter")) ? 
+            answer[idx++] = (map[i][0].equals("Enter")) ? 
                 hash.get(map[i][1]) + "님이 들어왔습니다." :
-                hash.get(map[i][1]) + "님이 나갔습니다.");    
+                hash.get(map[i][1]) + "님이 나갔습니다.";    
         }
         
-        return answer.toArray(new String[answer.size()]);
+        return answer;
     }
 }

427b02bbff16064140f6f489eea1661a366644c7
EliteZer0 <diana.yy.jeong@gmail.com>
2025-03-25 10:36:43 +0900

    [Week 26] BOJ 튜터-튜티 관계의 수/ 20분

--- week26/EliteZer0/boj_24542_튜터-튜티관계의수.java
+++ week26/EliteZer0/boj_24542_튜터-튜티관계의수.java
@@ -8,6 +8,9 @@ import java.util.StringTokenizer;
 public class boj_24542 {
     static int[] parent;
 
+    // 초기화 메서드로 분리했을 땐 75708kb 428ms 77732kb 432ms
+    // main 안에서 처리하면 78164kb 540ms 82488kb 540ms
+    // 원인을 찾지 못하였습니다...
     static void init(int n) {
         parent = new int[n+1];
         for (int i = 1; i <= n; i++) {
@@ -49,8 +52,14 @@ public class boj_24542 {
             union(a, b);
         }
 
+        // Map을 사용하여 각 연결 요소(집합)의 루트 노드를 키로, 해당 집합의 크기를 값으로 저장
+        // 배열 대신 Map을 사용한 이유:
+        // 루트 노드의 번호를 예측할 수 없음
+        // Map을 사용하면 실제 존재하는 루트 노드만 저장
+        // getOrDefault 메서드를 통해 값이 없을 경우 기본값(0) 반환 가능
         Map<Integer, Integer> sizeMap = new HashMap<>();
 
+        // 각 노드를 순회하며 자신이 속한 집합(연결 요소)의 루트를 찾고, 해당 집합의 크기를 1 증가
         for (int i = 1; i <= n; i++) {
             int root = find(i);
             sizeMap.put(root, sizeMap.getOrDefault(root, 0) + 1);
@@ -59,6 +68,7 @@ public class boj_24542 {
         long result = 1;
         final int MOD = 1_000_000_007;
 
+        // 매 연산마다 MOD 값으로 안 나누면 쓰레기 값 나올 수도 있음
         for (int size : sizeMap.values()) {
             result = (result * size) % MOD;
         }

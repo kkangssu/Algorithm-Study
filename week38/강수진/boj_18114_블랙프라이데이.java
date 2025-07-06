de14d7ea0b938efb22ae0c5eb3985bb24c28fe50
sujin <unikang16@gmail.com>
2025-07-07 00:11:34 +0900

    [Week38] BOJ 18114 블랙프라이데이 / 45분

--- week38/강수진/boj_18114_블랙프라이데이.java
+++ week38/강수진/boj_18114_블랙프라이데이.java
@@ -1,5 +1,7 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class boj_18114_블랙프라이데이 {
 
@@ -7,7 +9,7 @@ public class boj_18114_블랙프라이데이 {
     static int n;
     static long c;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
